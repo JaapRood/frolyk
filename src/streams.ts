41c112823700b0737cc4d850c2847438c4a7d9fa
Jaap Rood <jaap@jaaprood.nl>
2019-12-12 15:59:34 +0100

    Draft an implementation of TaskStreams to manage message streams

---
+++ src/streams.ts
@@ -0,0 +1,100 @@
+import { Consumer, Message } from 'kafkajs/types'
+import H from 'highland'
+
+class TaskStreams {
+    consumer: Consumer
+    streams: Array<{
+        topic: string,
+        partition: number,
+        stream: Highland.Stream<Message>
+    }>
+
+    constructor(consumer : Consumer) {
+        this.consumer = consumer
+        this.streams = []
+    }
+
+    stream({ topic, partition }: { topic: string, partition: number }) : Highland.Stream<Message> {
+        const stream = this.streams
+            .filter((topparStream) => topparStream.topic === topic && topparStream.partition === partition)    
+            .map(({ stream }) => stream)
+            .find(() => true) // pick the first matching stream
+
+        if (!stream) {
+            let newStream : Highland.Stream<Message> = H()
+            this.streams.push({
+                topic,
+                partition,
+                stream: newStream
+            })
+
+            return newStream
+        } else {
+            return stream
+        }
+    }
+
+    async start() {
+        const { consumer } = this
+        
+        await consumer.run({
+            autoCommit: false, // let us be in full control of committing offsets
+            eachBatchAutoResolve: false, // do our own local checkpointing
+            partitionsConsumedConcurrently: 4,
+
+            async eachBatch({ 
+                batch, 
+                resolveOffset: checkpoint, 
+                commitOffsetsIfNecessary: commitOffset, 
+                heartbeat, 
+                isRunning 
+            }) {
+                const stream = this.stream({ topic: batch.topic, partition: batch.partition })
+
+                const observingStream = stream.fork()
+
+                const streaming = observingStream.last().toPromise(Promise)
+
+                for (let message of batch.messages) {
+                    if (!isRunning() || stream.ended) break
+
+                    let onDrain
+                    let draining = new Promise((resolve, reject) => {
+                        onDrain = () => resolve()
+                        stream.once('drain', onDrain)
+                    })
+
+                    const more = stream.write({
+                        topic: batch.topic,
+                        partition: batch.partition,
+                        highWaterOffset: BigInt(batch.highWatermark),
+                        offset: BigInt(message.offset),
+
+                        ...message,
+
+                        timestamp: message.timestamp,
+                    })
+
+                    checkpoint(message.offset)
+                    await heartbeat()
+
+                    if (!more && isRunning() && !stream.ended) {
+                        await Promise.race([
+                            draining,
+                            streaming
+                        ])
+                    }
+
+                    stream.removeListener('drain', onDrain)
+                }
+
+                await heartbeat()
+                observingStream.destroy()
+            }
+        })
+    }
+}
+
+export default function create(consumer): TaskStreams {
+    return new TaskStreams(consumer)
+}
