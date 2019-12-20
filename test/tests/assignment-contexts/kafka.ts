9c869a22e23cd2692772b74c9e7770cd5c71a737
Jaap Rood <jaap@jaaprood.nl>
2019-12-20 15:02:42 +0100

    Verify AssignmentContext.Kafka applies processors to messages stream

--- test/tests/assignment-contexts/kafka.ts
+++ test/tests/assignment-contexts/kafka.ts
@@ -27,4 +27,82 @@ Tap.test('AssignmentContext.Kafka', async (t) => {
             stream
         })
     })
+
+    await t.test('processing pipeline', async (t) => {
+        let testAssignment, consumer, streams, stream
+        
+        t.beforeEach(async () => {
+            testAssignment = {
+                topic: `topic-${secureRandom()}`,
+                partition: 0,
+                group: `group-${secureRandom()}`
+            }
+            consumer = createConsumer({ groupId: testAssignment.group })
+            streams = createStreams(consumer)
+            stream = streams.stream({ topic: testAssignment.topic, partition: 0 })
+            await consumer.connect()
+            await consumer.subscribe({ topic: testAssignment.topic })
+            await streams.start()
+        })
+
+        t.afterEach(async () => {
+            if (consumer) await consumer.disconnect()
+        })
+
+        const testProcessor = (setupProcessors, assignment = testAssignment) => {
+            setupProcessors = [].concat(setupProcessors) // one or more processors
+            
+            return createKafkaAssignmentContext({
+                assignment,
+                consumer,
+                processors: setupProcessors,
+                stream
+            })
+        }
+
+        await t.test('returns a stream with all processors applied in order', async (t) => {
+            const testMessages = Array(100).fill({}).map(() => ({
+                value: `value-${secureRandom()}`,
+                key: `value-${secureRandom()}`,
+                partition: 0
+            }))
+
+            await produceMessages(testAssignment.topic, testMessages)
+            
+            const processMessageOne = spy(({ key, value }) => ({ 
+                key: key.toString(), 
+                value: value.toString() 
+            }))
+
+            const processMessageTwo = spy(( { key, value }) => ({
+                key: `processed-${key}`,
+                value: `processed-${value}`
+            }))
+            
+            const context = await testProcessor([
+                async (assignment) => {
+                    return processMessageOne
+                },
+                async (assignment) => {
+                    return processMessageTwo
+                }
+            ])
+
+            const processingResults = await context.stream
+                .take(testMessages.length)
+                .collect()
+                .toPromise(Promise)
+
+            t.equal(processMessageOne.callCount, testMessages.length)
+            t.equal(processMessageTwo.callCount, testMessages.length)
+            t.deepEqual(
+                processingResults,
+                testMessages.map(({ key, value }) => ({ 
+                    key: `processed-${key}`, 
+                    value: `processed-${value}`
+                }))
+            , 'applies processors to stream of messages')
+        })
+
+    })
 })
