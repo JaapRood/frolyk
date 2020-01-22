d637a3b2c752da2364a8fc660589b58712632f42
Jaap Rood <jaap@jaaprood.nl>
2020-01-22 14:51:52 +0100

    Perform resolving of logical offsets in assignment.seek, rather than forwarding logical offsets on

--- src/assignment-contexts/index.ts
+++ src/assignment-contexts/index.ts
@@ -18,7 +18,7 @@ export interface AssignmentContext {
 	committed(): Promise<OffsetAndMetadata>,
 	isEmpty(): Promise<boolean>,
 	log(tags, payload): any,
-	seek(offset: string | Long | LogicalOffset | LogicalLiteralOffset): void,
+	seek(offset: string | Long | LogicalOffset | LogicalLiteralOffset): Promise<void>,
 	send(messages: NewMessage | NewMessage[]): Promise<ProducedMessageMetadata[]>,
 	watermarks(): Promise<Watermarks>
 }
